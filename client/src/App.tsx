6ac9216c85eef1fe1c33447a5e000634d3a79ee9
Bernardo <bgbaine@gmail.com>
2024-10-29 17:43:02 -0300

    feat: implement form skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
-function App() {
+import Disciplines from "./components/Disciplines";
 
+
+function App() {
   return (
     <>
-      <h1 className='text-4xl text-red-500'>Welcome to teste-dti</h1>
+      <h1 className="text-4xl text-red-500">Olá, Carlos</h1>
+      <form action="">
+        <div>
+          <label htmlFor="nome">Nome do aluno: </label>
+          <input type="text" id="nome" required />
+        </div>
+        <Disciplines />
+        <div>
+          <label htmlFor="frequencia">Frequência do aluno: </label>
+          <input type="number" id="frequencia" min="0" max="100" required />
+        </div>
+        <div>
+          <button type="reset">Limpar</button>
+          <button type="submit">Cadastrar</button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
