e6fd755f648e58b3de22911ac4858234cc4585eb
Bernardo <bgbaine@gmail.com>
2024-10-29 20:27:14 -0300

    feat: add support for student data handling with react-hook-form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,39 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { StudentData } from "./types/types";
 import Disciplines from "./components/Disciplines";
 
 
 function App() {
+  const { register, handleSubmit, setFocus, reset } = useForm<StudentData>();
+
+  const submitStudentData: SubmitHandler<StudentData> = (data) => {
+    reset();
+
+    setFocus("nome");
+
+    console.log(data.nome);
+
+  };
+
   return (
     <>
       <h1 className="text-4xl text-red-500">Olá, Carlos</h1>
-      <form action="">
+      <form onSubmit={handleSubmit(submitStudentData)}>
         <div>
           <label htmlFor="nome">Nome do aluno: </label>
-          <input type="text" id="nome" required />
+          <input type="text" id="nome" required {...register("nome")} />
         </div>
-        <Disciplines />
+        <Disciplines register={register} />
         <div>
           <label htmlFor="frequencia">Frequência do aluno: </label>
-          <input type="number" id="frequencia" min="0" max="100" required />
+          <input
+            type="number"
+            id="frequencia"
+            min="0"
+            max="100"
+            required
+            {...register("frequencia")}
+          />
         </div>
         <div>
           <button type="reset">Limpar</button>
