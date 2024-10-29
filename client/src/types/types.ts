4545a20674a2521fff60d3933aaa172bce57c6a9
Bernardo <bgbaine@gmail.com>
2024-10-29 20:19:27 -0300

    feat: implement StudentData interface

---
+++ client/src/types/types.ts
@@ -0,0 +1,9 @@
+export interface StudentData {
+    nome: string;
+    disciplina1: number;
+    disciplina2: number;
+    disciplina3: number;
+    disciplina4: number;
+    disciplina5: number;
+    frequencia: number;
+}
