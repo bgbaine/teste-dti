e691be7f8d730307ff1a41f60bfdf77a6f27eec7
Bernardo <bgbaine@gmail.com>
2024-10-30 06:47:22 -0300

    feat: implement StudentTableProps interface

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,25 +1,31 @@
 export interface StudentData {
-    id: number,
-    nome: string;
-    nota1: number;
-    nota2: number;
-    nota3: number;
-    nota4: number;
-    nota5: number;
-    frequencia: number;
+  id: number;
+  nome: string;
+  nota1: number;
+  nota2: number;
+  nota3: number;
+  nota4: number;
+  nota5: number;
+  frequencia: number;
 }
 
 export interface ClassAverageBySubject {
-    gradeAverage1: number;
-    gradeAverage2: number;
-    gradeAverage3: number;
-    gradeAverage4: number;
-    gradeAverage5: number;
+  gradeAverage1: number;
+  gradeAverage2: number;
+  gradeAverage3: number;
+  gradeAverage4: number;
+  gradeAverage5: number;
 }
 
 export interface ClassData {
-    students: StudentData[];
-    classAverageBySubject: ClassAverageBySubject;
-    aboveAverage: string[];
-    lowFrequency: string[];
+  students: StudentData[];
+  classAverageBySubject: ClassAverageBySubject;
+  aboveAverage: string[];
+  lowFrequency: string[];
+}
+
+export interface StudentTableProps {
+  classData: ClassData;
+  editStudent: (id: number) => void;
+  deleteStudent: (id: number) => void;
 }
