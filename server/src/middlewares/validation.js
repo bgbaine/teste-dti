73f62f98f93959a54722d2df0d0ec7553584bd7b
Bernardo <bgbaine@gmail.com>
2024-10-29 23:59:43 -0300

    feat: add server-side validation for new student

---
+++ server/src/middlewares/validation.js
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+export const schemaAluno = z.object({
+  nome: z.string().min(1, "Insira um nome"),
+  nota1: z
+    .number()
+    .int()
+    .min(0)
+    .max(10, "Nota deve ser um inteiro entre 0 e 10"),
+  nota2: z
+    .number()
+    .int()
+    .min(0)
+    .max(10, "Nota deve ser um inteiro entre 0 e 10"),
+  nota3: z
+    .number()
+    .int()
+    .min(0)
+    .max(10, "Nota deve ser um inteiro entre 0 e 10"),
+  nota4: z
+    .number()
+    .int()
+    .min(0)
+    .max(10, "Nota deve ser um inteiro entre 0 e 10"),
+  nota5: z
+    .number()
+    .int()
+    .min(0)
+    .max(10, "Nota deve ser um inteiro entre 0 e 10"),
+  frequencia: z
+    .number()
+    .int()
+    .min(0)
+    .max(100, "Frequência deve ser um inteiro entre 0 e 100"),
+});
+
+export const validateAluno = (req, res, next) => {
+  try {
+    schemaAluno.parse(req.body);
+    console.log("passou aqui");
+    
+    next();
+  } catch (error) {
+    res.status(400).json({ errors: error.errors });
+  }
+};
