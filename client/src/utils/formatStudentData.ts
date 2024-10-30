ecf7ffdd964fc1edee9a2cc7aeeb5ff638154bd6
Bernardo <bgbaine@gmail.com>
2024-10-30 00:31:17 -0300

    feat: add function to format student data for backend posting

---
+++ client/src/utils/formatStudentData.ts
@@ -0,0 +1,15 @@
+import { StudentData } from "../types/types";
+
+const formatStudentData = (data: StudentData) => {
+  return {
+    nome: data.nome,
+    nota1: Number(data.nota1),
+    nota2: Number(data.nota2),
+    nota3: Number(data.nota3),
+    nota4: Number(data.nota4),
+    nota5: Number(data.nota5),
+    frequencia: Number(data.frequencia),
+  };
+};
+
+export default formatStudentData;
