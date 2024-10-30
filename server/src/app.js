54abe07b280de9e02a7f0ef6bb44a0af65d4faa6
Bernardo <bgbaine@gmail.com>
2024-10-30 00:07:52 -0300

    feat: add cors support and import index.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes/index.js';
+import cors from 'cors';
+
 
 dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
 
 app.use(express.json());
+app.use(cors(corsOptions));
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
