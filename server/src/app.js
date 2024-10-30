fcc2a80b1ae2ef88a0f5436e20ee0395dc31ccc0
Bernardo <bgbaine@gmail.com>
2024-10-30 09:17:21 -0300

    fix: add cors support for multiple ports

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,15 @@ import cors from 'cors';
 const app = express();
 const port = 3001;
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'http://localhost:5176',
+    'http://localhost:5177',
+    'http://localhost:5178',
+    'http://localhost:5179',
+  ],
 };
 
 app.use(express.json());
