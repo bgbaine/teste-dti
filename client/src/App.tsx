9583424948be28e93ba026d8d86df7875143a669
Bernardo <bgbaine@gmail.com>
2024-10-30 00:46:15 -0300

    feat: add post interaction with backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,38 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { StudentData } from "./types/types";
 import Disciplines from "./components/Disciplines";
-
+import formatStudentData from "./utils/formatStudentData";
 
 function App() {
   const { register, handleSubmit, setFocus, reset } = useForm<StudentData>();
 
-  const submitStudentData: SubmitHandler<StudentData> = (data) => {
+  const submitStudentData: SubmitHandler<StudentData> = async (data) => {
     reset();
 
     setFocus("nome");
+    console.log(data);
 
-    console.log(data.nome);
+    const formattedData = formatStudentData(data);
+    console.log(formattedData);
+    
+    try {
+      const response = await fetch("http://localhost:3001/aluno", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formattedData),
+      });
 
+      if (!response.ok) {
+        throw new Error(`O servidor respondeu, mas com um erro. Resposta: ${response.statusText}`);
+      }
+
+      const result: any = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error("Houve um problema enviando os dados para o servidor. Erro:", error);
+    }
   };
 
   return (
