290125357cc43aff9088961c091d44413da12732
Bernardo <bgbaine@gmail.com>
2024-10-30 04:30:40 -0300

    refactor: renaming for proper standard

--- client/src/utils/formatStudentData.ts
+++ client/src/utils/formatStudentData.ts
@@ -1,15 +1,15 @@
 import { StudentData } from "../types/types";
 
-const formatStudentData = (data: StudentData) => {
+const FormatStudentData = (data: StudentData) => {
   return {
-    nome: data.nome,
-    nota1: Number(data.nota1),
-    nota2: Number(data.nota2),
-    nota3: Number(data.nota3),
-    nota4: Number(data.nota4),
-    nota5: Number(data.nota5),
+    name: data.nome,
+    grade1: Number(data.nota1),
+    grade2: Number(data.nota2),
+    grade3: Number(data.nota3),
+    grade4: Number(data.nota4),
+    grade5: Number(data.nota5),
     frequencia: Number(data.frequencia),
   };
 };
 
-export default formatStudentData;
+export default FormatStudentData;
