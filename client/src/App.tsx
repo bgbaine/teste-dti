3ffb682cf72e46cac7d1151e5b91a95b2203e4c8
Bernardo <bgbaine@gmail.com>
2024-10-30 06:50:18 -0300

    feat: add student table component and some minor standardization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,15 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { ClassData, StudentData } from "./types/types";
 import Disciplines from "./components/Disciplines";
 import formatStudentData from "./utils/FormatStudentData";
-import CalculateClassAverage from "./utils/CalculateClassAverage";
-import CalculateStudentAverage from "./utils/CalculateStudentAverage";
 import { useEffect, useState } from "react";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
+import StudentTable from "./components/StudentTable"; // Adjust the import path as needed
 
 function App() {
+  const [nome, setNome] = useState("Carlos");
   const { register, handleSubmit, setFocus, reset } = useForm<StudentData>();
+  const [open, setOpen] = useState(false);
   const [classData, setClassData] = useState<ClassData>();
 
   useEffect(() => {
@@ -17,7 +20,7 @@ function App() {
 
   const fetchClassData = async () => {
     try {
-      const response = await fetch("http://localhost:3001/alunos");
+      const response = await fetch("http://localhost:3001/students");
 
       if (!response.ok) {
         throw new Error(
@@ -41,7 +44,7 @@ function App() {
 
     const formattedData = formatStudentData(data);
     try {
-      const response = await fetch("http://localhost:3001/alunos", {
+      const response = await fetch("http://localhost:3001/students", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -55,6 +58,7 @@ function App() {
         );
       }
 
+      setOpen(false);
       fetchClassData();
     } catch (error) {
       console.error(
@@ -69,156 +73,115 @@ function App() {
   };
 
   const deleteStudent = (id: number) => {
+    /* try {
+      fetch(`http://localhost:3001/students/${id}`, {
+        method: "DELETE",
+      });
+
+      fetchClassData();
+    } catch (error) {
+      console.error(
+        "Houve um problema enviando os dados para o servidor. Erro:",
+        error
+      );
+    } */
     alert(`you pressed ${id}`);
+  };
+
+  const openForm = () => {
+    setOpen(true);
+    reset();
+    setFocus("nome");
   };
 
   return (
     <>
-      <header className="flex justify-center items-center">
-        <h1 className="text-4xl text-red-500">Olá, Carlos</h1>
+      <header className="flex flex-col items-center mt-4 bg-gray-100 p-4 rounded-lg shadow-md">
+        <h1 className="text-4xl font-bold text-blue-500 mb-4">Olá, {nome}</h1>
+        <p className="text-lg text-gray-700">Bem-vindo ao teste-dti!</p>
       </header>
       <main className="flex flex-col justify-center items-center">
-        {classData && (
-          <div>
-            <h2 className="text-center">Estudantes Cadastrados:</h2>
-            <div className="overflow-x-auto">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Nome
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Nota 1
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Nota 2
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Nota 3
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Nota 4
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Nota 5
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Frequência
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Média
-                    </th>
-                    <th className="px-4 py-3 text-left text-xs sm:text-sm md:text-base font-medium text-gray-500 uppercase tracking-wider">
-                      Ações
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {classData.students.map((student) => (
-                    <tr key={student.id} className="hover:bg-gray-100">
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.nome}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.nota1}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.nota2}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.nota3}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.nota4}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.nota5}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {student.frequencia}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        {CalculateStudentAverage(student)}
-                      </td>
-                      <td className="px-4 py-4 whitespace-nowrap">
-                        <button
-                          onClick={() => editStudent(student.id)}
-                          className="text-blue-600 hover:text-blue-900"
-                        >
-                          Edit
-                        </button>
-                        <button
-                          onClick={() => deleteStudent(student.id)}
-                          className="text-red-600 hover:text-red-900 ml-2"
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-                <tfoot>
-                  <tr className="bg-gray-100">
-                    <td className="px-4 py-4 font-bold">
-                      Média da turma por disciplina
-                    </td>
-                    <td className="px-4 py-4">
-                      {classData.classAverageBySubject.gradeAverage1}
-                    </td>
-                    <td className="px-4 py-4">
-                      {classData.classAverageBySubject.gradeAverage2}
-                    </td>
-                    <td className="px-4 py-4">
-                      {classData.classAverageBySubject.gradeAverage3}
-                    </td>
-                    <td className="px-4 py-4">
-                      {classData.classAverageBySubject.gradeAverage4}
-                    </td>
-                    <td className="px-4 py-4">
-                      {classData.classAverageBySubject.gradeAverage5}
-                    </td>
-                    <td className="px-4 py-4" colSpan={2}></td>{" "}
-                    <td className="px-4 py-4" colSpan={2}></td>{" "}
-                  </tr>
-                  <tr className="bg-gray-200">
-                    <td className="px-4 py-4 font-bold">
-                      Média Geral da turma
-                    </td>
-                    <td className="px-4 py-4" colSpan={6}>
-                      {CalculateClassAverage(classData.classAverageBySubject)}
-                    </td>
-                    <td className="px-4 py-4"></td>{" "}
-                    <td className="px-4 py-4"></td>{" "}
-                  </tr>
-                </tfoot>
-              </table>
-            </div>
-          </div>
+        {classData ? (
+          <StudentTable
+            classData={classData}
+            editStudent={editStudent}
+            deleteStudent={deleteStudent}
+          />
+        ) : (
+          <>
+            <h2 className="text-xl mt-4 text-center">
+              Você não possui nenhum aluno cadastrado no momento
+            </h2>
+            <h3 className="text-lg mt-3">
+              Insira um clicando no botão abaixo!
+            </h3>
+          </>
         )}
+        <button
+          onClick={openForm}
+          className="px-4 py-3 mt-6 mb-8 bg-blue-600 text-white rounded-md hover:bg-blue-700 font-semibold shadow-gray-500 shadow-md"
+        >
+          Inserir Aluno
+        </button>
+        <Modal open={open} onClose={() => setOpen(false)} center>
+          <form
+            onSubmit={handleSubmit(submitStudentData)}
+            className="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-lg"
+          >
+            <h2 className="text-xl font-semibold mb-4">Preencha os campos</h2>
 
-        <form onSubmit={handleSubmit(submitStudentData)}>
-          <div>
-            <label htmlFor="nome">Nome do aluno: </label>
-            <input type="text" id="nome" required {...register("nome")} />
-          </div>
-          <Disciplines register={register} />
-          <div>
-            <label htmlFor="frequencia">Frequência do aluno: </label>
-            <input
-              type="number"
-              id="frequencia"
-              min="0"
-              max="100"
-              required
-              {...register("frequencia")}
-            />
-          </div>
-          <div>
-            <button type="reset">Limpar</button>
-            <button type="submit">Cadastrar</button>
-          </div>
-        </form>
+            <div className="mb-4">
+              <label
+                htmlFor="nome"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Nome do aluno:
+              </label>
+              <input
+                type="text"
+                id="nome"
+                required
+                {...register("nome")}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              />
+            </div>
+
+            <Disciplines register={register} />
+
+            <div className="mb-4">
+              <label
+                htmlFor="frequencia"
+                className="block text-sm font-medium text-gray-700 mt-3"
+              >
+                Frequência do aluno:
+              </label>
+              <input
+                type="number"
+                id="frequencia"
+                min="0"
+                max="100"
+                required
+                {...register("frequencia")}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="flex justify-between mt-6">
+              <button
+                type="reset"
+                className="w-full bg-red-600 text-white py-2 rounded-md hover:bg-red-700 font-semibold shadow-gray-500 shadow-md"
+              >
+                Limpar
+              </button>
+              <button
+                type="submit"
+                className="w-full ml-2 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 font-semibold shadow-gray-500 shadow-md"
+              >
+                Inserir
+              </button>
+            </div>
+          </form>
+        </Modal>
       </main>
     </>
   );
